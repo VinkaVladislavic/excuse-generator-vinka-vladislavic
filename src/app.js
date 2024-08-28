345f7fb4b691168b9d7a7de09f3828deae16b9fa
Vinka Vladislavic <123342469+VinkaVladislavic@users.noreply.github.com>
2024-08-28 01:14:22 +0000

    excuse generaator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+let excuseOptions = [who, action, what, when];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse = [];
+  for (let i = 0; i < excuseOptions.length; i++) {
+    excuse[i] = randomExcuse(excuseOptions[i]);
+  }
+  document.getElementById("excuse").innerText = excuse.join(" ");
 };
+
+function randomExcuse(array) {
+  let randomNumber = Math.floor(Math.random() * array.length);
+  return array[randomNumber];
+}
